bad938bd3984dfa1c694d886edb23971fc2287d4
QuerZamora <114662464+QuerZamora@users.noreply.github.com>
2025-01-27 19:21:43 +0100

    #1 input field to conenct to room and user info

--- testconnection.js
+++ testconnection.js
@@ -1,16 +1,36 @@
-
-
-var server = new SillyClient();
-server.connect( "ws://172.201.217.153:80", "CHAT5_roomnametest");
-
-//this method is called when the server accepts the connection (no ID yet nor info about the room)
-server.on_connect = function(){
-  console.log("Connecting to server");
-};
-
-server.on_ready = ( my_id ) =>
-  {
-    console.log("Connected to server with ID: " + my_id);
-  }
-
 console.log("testconnection.js loaded");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const connectBtn = document.getElementById("connect-btn");
+  const roomInput = document.getElementById("room");
+
+  connectBtn.addEventListener("click", () => {
+      const room = roomInput.value.trim();
+
+      if (!room) {
+          alert("Room Name is required!");
+          return;
+      }
+
+      var server = new SillyClient();
+      server.connect( "ws://172.201.217.153:80", `CHAT5_${room}`);
+
+      server.on_ready = (my_id) => {
+          console.log("Connected to server with ID: " + my_id);
+      };
+
+      server.on_room_info = (info) => {
+        console.log(info);
+      }
+      
+      server.on_user_connected = id => {
+          console.log(`User connected: ${id}`);
+      };
+
+      server.on_user_disconnected = id => {
+          console.log(`User disconnected: ${id}`);
+      };
+    }
+  )
+ }
+);
