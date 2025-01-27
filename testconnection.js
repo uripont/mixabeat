3b0cbf0e6631ba5ccbd425f291a64dd0385f0077
uripont-upf <oriol.pont01@estudiant.upf.edu>
2025-01-27 19:03:44 +0100

    getting id from connection to server #1

--- testconnection.js
+++ testconnection.js
@@ -5,7 +5,12 @@ server.connect( "ws://172.201.217.153:80", "CHAT5_roomnametest");
 
 //this method is called when the server accepts the connection (no ID yet nor info about the room)
 server.on_connect = function(){
-  console.log("Connected to server");
+  console.log("Connecting to server");
 };
+
+server.on_ready = ( my_id ) =>
+  {
+    console.log("Connected to server with ID: " + my_id);
+  }
 
 console.log("testconnection.js loaded");
