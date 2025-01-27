51003e4cdd9538314d697e04fd612adac8bb0fa1
QuerZamora <114662464+QuerZamora@users.noreply.github.com>
2025-01-27 20:07:13 +0100

    #2 send messages in a room

--- testconnection.js
+++ testconnection.js
@@ -1,11 +1,12 @@
-console.log("testconnection.js loaded");
-
 document.addEventListener("DOMContentLoaded", () => {
   const connectBtn = document.getElementById("connect-btn");
   const roomInput = document.getElementById("room");
+  const messageInput = document.getElementById("message");
+  const sendMessageBtn = document.getElementById("send-message-btn");
 
   connectBtn.addEventListener("click", () => {
       const room = roomInput.value.trim();
+      
 
       if (!room) {
           alert("Room Name is required!");
@@ -18,7 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
       server.on_ready = (my_id) => {
           console.log("Connected to server with ID: " + my_id);
       };
-
+    
       server.on_room_info = (info) => {
         console.log(info);
       }
@@ -30,7 +31,19 @@ document.addEventListener("DOMContentLoaded", () => {
       server.on_user_disconnected = id => {
           console.log(`User disconnected: ${id}`);
       };
-    }
-  )
- }
-);
+
+
+      sendMessageBtn.addEventListener("click", () => {
+        const message = messageInput.value.trim();
+        
+        if (message) {
+            server.sendMessage(`CHAT5_${room}`, message);  
+            console.log("Message sent: " + message);
+            //messageInput.value = "";  
+        } else {
+            console.log("Message cannot be empty!");
+        }
+      });
+
+  });
+});
