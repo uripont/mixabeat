f24b563faeff3d1989856e0a12931ec99a0cc3d2
uripont-upf <oriol.pont01@estudiant.upf.edu>
2025-03-01 20:00:33 +0100

    cleaner logger

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -1,27 +1,32 @@
-const winston = require('winston');
+const winston = require('winston'); // npm install winston
 const path = require('path');
 const fs = require('fs');
 
-// Ensure log directory exists
-const logDir = 'log';
-if (!fs.existsSync(logDir)) {
-    fs.mkdirSync(logDir);
+function ensureLogDirectoryExists(logDir) {
+    if (!fs.existsSync(logDir)) {
+        fs.mkdirSync(logDir);
+    }
 }
 
-// Create logger instance
-const logger = winston.createLogger({
-    format: winston.format.simple(),
-    transports: [
-        new winston.transports.Console({
-            format: winston.format.combine(
-                winston.format.colorize(),
-                winston.format.simple()
-            )
-        }),
-        new winston.transports.File({
-            filename: path.join(logDir, 'server.log')
-        })
-    ]
-});
+function createLogger(logDir) {
+    ensureLogDirectoryExists(logDir);
 
+    return winston.createLogger({
+        format: winston.format.simple(),
+        transports: [
+            new winston.transports.Console({
+                format: winston.format.combine(
+                    winston.format.colorize(),
+                    winston.format.simple()
+                )
+            }),
+            new winston.transports.File({
+                filename: path.join(logDir, 'server.log')
+            })
+        ]
+    });
+}
+
+const logDir = 'log';
+const logger = createLogger(logDir);
 module.exports = logger;
