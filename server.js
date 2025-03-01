ccdbeea6421587173931f5def821b69563ce6cf7
uripont-upf <oriol.pont01@estudiant.upf.edu>
2025-03-01 21:21:02 +0100

    (wip) until updating the backend, all auth are served from "/", not from "/auth"

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 
 // Routes
-app.use('/auth', authRoutes);
+app.use('/', authRoutes);
 
 // Create HTTP server
 const httpServer = app.listen(3000, () => {
